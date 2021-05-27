84adb57b8e661e042445081898818fa14b828ead
Abhishek Sengupta <senabhishek44@gmail.com>
2021-05-28 00:02:22 +0530

    Added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DiabetesPredictionApp",
+    version="0.0.1",
+    description="Diabetes Prediction- MLOps Project",
+    author="Abhishek-Sengupta",
+    packages=find_packages()
+)
+
+
